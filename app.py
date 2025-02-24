87239ff08de85a6d776b371e295c2d4735cbfd6d
Jianping <pinkwjp@gmail.com>
2025-02-24 16:11:56 -0500

    update, working

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # python -m app
 
 from time import sleep
+from pathlib import Path
 
 from src import utils
 from importlib import reload
@@ -8,7 +9,8 @@ reload(utils)  # make sure updates on utils funcs in effect
 
 from src.utils import get_driver
 from src.utils import (start_pyautogui, at_target_page, 
-                       click_checkbox_on_verification_page)
+                       click_checkbox_on_verification_page,
+                       locate_image_center_on_screen, perform_click)
 
 
 
@@ -21,14 +23,19 @@ def main():
         driver.uc_open_with_reconnect(url, 10)
         driver.maximize_window()
         sleep(5) # wait long enough for the cloudflare checkbox to appear
+        checkbox_image = './images/checkbox.png' 
+        assert Path(checkbox_image).exists()
         attempt = 0
         while attempt < 3:
-            if click_checkbox_on_verification_page():
-                if at_target_page():
-                    print('bypass verification, at target page!')
-                    break
+            if target_center := locate_image_center_on_screen(checkbox_image):
+                if perform_click(target_center.x, target_center.y):
+                    print('clicked checkbox.')
+                    sleep(4) # wait for page to refresh
+                    if not locate_image_center_on_screen(checkbox_image):
+                        print('bypass verification!')
+                        break
             else:
-                print('failed to click checkbox.')
+                print('failed to locate checkbox.')
             attempt += 1
 
 
