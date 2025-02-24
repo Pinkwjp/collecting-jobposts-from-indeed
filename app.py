f223b590dd6b502002465976b6dfebdd1a31ef39
Jianping <pinkwjp@gmail.com>
2025-02-24 15:07:11 -0500

    update main()

--- app.py
+++ app.py
@@ -1,4 +1,30 @@
 
+from time import sleep
 
 from src.utils import get_driver
+from src.utils import (start_pyautogui, at_target_page, 
+                       click_checkbox_on_verification_page)
 
+
+
+def main():
+    start_pyautogui() 
+    with get_driver(undetectable=True, incognito=True) as driver:  
+        url = 'https://ca.indeed.com/'
+        driver.uc_open_with_reconnect(url, 10)
+        driver.maximize_window()
+        sleep(5) # wait long enough for the cloudflare checkbox to appear
+        attempt = 0
+        while attempt < 3:
+            if click_checkbox_on_verification_page():
+                if at_target_page():
+                    print('bypass verification, at target page!')
+                    break
+            else:
+                print('failed to click checkbox.')
+            attempt += 1
+
+
+
+if __name__ == '__main__':
+    main()
