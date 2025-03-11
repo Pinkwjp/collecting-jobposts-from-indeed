6a87c230b739c12e6c0d17f4be7d569a7fe83234
ping <pinkwjp@gmail.com>
2025-03-10 22:06:49 -0400

    add comments

--- app.py
+++ app.py
@@ -1,5 +1,37 @@
 # python -m app
 
+
+# NOTE: when encounter 'Xlib.error.DisplayConnectionError'
+# in Linux terminal run: 'xhost +' 
+# response: 'access control disabled, clients can connect from any host'
+
+# NOTE: not able to handld cloudflare with builtin seleniumbase methods
+
+# NOTE: not able to locate cloudflare iframe with driver.find_element
+
+# NOTE: on Fedora, need Python 3.10 to be compatible with pyautogui and its dependencies
+
+# NOTE: seems like pyautogui.locateCenterOnScreen prefer simple image with simple lines
+
+# NOTE: As of pyautogui version 0.9.41, if the locate functions can’t find the provided image, 
+# they’ll raise ImageNotFoundException instead of returning None.
+
+# NOTE:
+# with SB(undetected=True, incognito=True) as driver:
+# with DriverContext(uc=True) as driver: 
+# both these context managers seem not fully compatible with Fedora, 
+# causing error: "X11 display failed! Will use regular xvfb!"
+
+# NOTE: on Fedora, need the followings libs for pyautogui
+# sudo dnf install scrot
+# sudo dnf install python3-tkinter
+# sudo dnf install python3-devel
+
+# use pipenv to generate requirements.txt 
+# pipenv run pip freeze > requirements.txt   
+
+
+
 from time import sleep
 from pathlib import Path
 
@@ -12,9 +44,6 @@ from src.utils import (start_pyautogui,
                        locate_image_center_on_screen, 
                        perform_click)
 
-
-# use pipenv to generate requirements.txt 
-# pipenv run pip freeze > requirements.txt    
 
 
 def handle_verification() -> bool:
@@ -47,7 +76,6 @@ def main():
             print('failed to handle cloudflare verification, exiting...')
             return
         
-
 
 
 
