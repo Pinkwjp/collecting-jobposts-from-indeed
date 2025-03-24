4cb5fc5e8a8153862290dfa5808a34a59385b51f
ping <pinkwjp@gmail.com>
2025-03-24 13:17:51 -0400

    add selectors

--- src/selectors.py
+++ src/selectors.py
@@ -20,3 +20,10 @@ LANGUAGE_MENU = "ul[id='filter-lang-menu']"
 # JOB_LANGUAGE_ITEM = Locator(By.TAG_NAME, value="a")  English
 
 
+# css selectors for extraction with BeautifulSoup 
+JOB_TITLE = "h2[data-testid='simpler-jobTitle']"
+JOB_DETAILS = "div[id='jobDetailsSection']"
+JOB_LOCATION = "div[id='jobLocationWrapper']" 
+JOB_BENEFIT = "div[id='benefits']"
+JOB_DESCRIPTION = "div[id='jobDescriptionText']"
+
