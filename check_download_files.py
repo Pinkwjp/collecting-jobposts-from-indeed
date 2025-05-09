f654b63c8e05ac4ab2a5e6b5a5c0e099b2ab3be1
ping <pinkwjp@gmail.com>
2025-05-09 16:10:01 -0400

    update

--- check_download_files.py
+++ check_download_files.py
@@ -5,10 +5,12 @@ import shelve
 if __name__ == '__main__':
     # python -m check_download_files 
     
+    # jobposts/id_to_filename_db
+    # jobposts/id_to_filename_remote_db
         
-    with shelve.open('./jobposts/id_to_filename_db') as db:
+    with shelve.open('./jobposts/id_to_filename_remote_db') as db:
         id_to_filename = dict(db)
-        print(id_to_filename)
+        print(len(id_to_filename))
 
 
 
